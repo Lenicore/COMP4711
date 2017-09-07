69ec932ddbf3d8ed4bfdaf3613163e44bacbb660
lenic <lenic.zhang@gmail.com>
2017-09-07 14:06:29 -0700

    add comments

--- Student.php
+++ Student.php
@@ -12,7 +12,7 @@
  * @author lenic
  */
 class Student {
-    //put your code here
+    //default constructor
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,14 +20,17 @@ class Student {
         $this->grades = array();
     }
     
+    // add email function
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    // add grade funtion
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //calculate grade average funtion
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -35,6 +38,7 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //format out put
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
